4d9fc6d2056518518367a78bb6b2451c78d80540
Josh Bosley <broskisworld@gmail.com>
2024-07-21 22:22:33 -0600

    add assets

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var proxy = require('html2canvas-proxy');
 var express = require('express');
+const port = process.env.PORT || 4848;
 
 var app = express();
-app.use('/', proxy());
+app.use('/proxy/', proxy());
+app.use('/assets/',express.static('../html2canvas/dist/'));
+
+app.listen(port, () => {
+  console.log(`Proxy listening on port ${port}`)
+});
